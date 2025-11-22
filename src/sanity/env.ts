cc0aef2b24b462de7cb13d515e5a55345e4448bc
AmiralJPB-Kti <kti@badie.eu>
2025-11-22 21:39:44 +0000

    fix: Hardcode Sanity project ID and dataset as default in env.ts for Vercel build

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -2,10 +2,12 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-11-13'
 
 export const dataset =
+
   process.env.NEXT_PUBLIC_SANITY_DATASET || 'production' // Ajout d'une valeur par défaut pour le build Vercel
 
 export const projectId =
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'votre-sanity-project-id' // Ajout d'une valeur par défaut pour le build Vercel. IMPORTANT: configurez la vraie valeur sur Vercel!
+
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'h0hullke' // Ajout d'une valeur par défaut pour le build Vercel. IMPORTANT: configurez la vraie valeur sur Vercel!
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
